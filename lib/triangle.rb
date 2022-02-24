bc98a742f8b26dacf46ccf642cf615bbcdd69d89
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-24 18:45:10 -0500

    commit tree f267fda609e552cac6f7bb906cb943f6b4b3e6b6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :l1, :l2, :l3, :type
+
+  def initialize (l1, l2, l3)
+    @l1=l1
+    @l2=l2
+    @l3=l3
+  end
+
+  def kind
+    @self.type=
+    if l1==l2 && l1==l3
+      "equilateral"
+    elsif l1==l2 || l1==l3
+      "isosceles"
+    else 
+      "scalene"
+    end
+  end
 end
+
+binding.pry
+0
